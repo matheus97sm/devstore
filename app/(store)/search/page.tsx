1573bfd4ca401f983da033b9c69ab1ae0b6cf0c8
Matheus Fonseca <matheus97sm@gmail.com>
2023-12-04 12:00:53 -0300

    first commit

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,9 @@
+export default async function Search() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return (
+    <div>
+      <h1>Hello World - Search</h1>
+    </div>
+  );
+}
